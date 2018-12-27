0a329bc337ce3efda40d0b24d34baa1865c1ad45
Yaoyu  Cui <yaoyucui@Yaoyus-MacBook-Pro.local>
2018-12-28 07:41:32 +0800

    HDF5_Face_Crop_updates

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from Data_Prep import data_clean,identity_name,train_test_split,shrinkage
+from Data_Store import Initialize_hdf5
+from Face_Crop import face_crop_to_array
 
 if __name__ == "__main__":
-    error_list = data_clean("YTFErrors.csv")
-    name_list = identity_name("frame_images_DB", error_list)
+    error_list = data_clean("/Users/yaoyucui/Works/Smith/Deep Learning/Youtube Dataset/YTFErrors.csv")
+    name_list = identity_name("/Users/yaoyucui/Works/Smith/Deep Learning/Youtube Dataset/frame_images_DB", error_list)
     train_set,test_set = train_test_split(name_list)
+
     train_addrs = shrinkage(train_set,48) # take 48 frames per person for the training data
     test_addrs = shrinkage(test_set,10) # take 10 frames per person for the training data
-    print(test_addrs[1])
+
+    # Initialize HDF5 database
+    Initialize_hdf5(train_addrs,test_addrs,120,120)
+
+    # Crop the faces
+    train_data = 0
+    for file in train_addrs:
+        face_img = face_crop_to_array(file,120,120)
+        train_data += 1
+    # print(train_data)
+    # print(face_img)
+    # print(hdf5_file["train_labels"][0])d
