4939f970accbbbdbab91fa703f4ba23732364fcb
Yaoyu  Cui <yaoyucui@Yaoyus-MacBook-Pro.local>
2019-01-11 14:08:32 -0500

    replace talos with hand written

--- Hyper-tune_modi.py
+++ Hyper-tune_modi.py
@@ -1,6 +1,3 @@
-# import talos
-import talos as ta
-
 # pylint: disable=no-member
 import h5py
 from keras import utils
@@ -9,76 +6,79 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import optimizers
-from talos.model.early_stopper import early_stopper
-
-def CNN_model (x_train, y_train, x_val, y_val, params):
-    model = Sequential()
-    model.add(Conv2D(params['first_neuron'], kernel_size=(3, 3),
-                    activation ='relu',
-                    input_shape =x_train.shape[1],
-                    padding='same',
-                    dilation_rate=2))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(params['first_neuron']*2, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(y_train.shape[1], activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                optimizer=optimizers.Adadelta(),
-                metrics=['accuracy'])
-    
-    history = model.fit(x_train, y_train,
-                        batch_size = params['batch_size'],
-                        epochs = epochs,
-                        verbose = 2,
-                        validation_data=[x_val, y_val],
-                        callbacks=early_stopper(5, mode='strict')
-                        )
-
-    return (history,model)
-
+from keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     # Read data from hdf5 file
-    # Data from independent .py file:
-    # main_data_store.py
+        # Data from independent .py file:
+        # main_data_store.py
     hdf5_path = 'dataset.hdf5'
     hdf5_file = h5py.File(hdf5_path, "r")
 
+    X_train,X_test,y_train,y_test = train_test_split(hdf5_file["img"][...],hdf5_file['labels'][...],test_size=0.2)
+
+
     # Convert labels to One-Hot-Encoding
     encoder = LabelEncoder()
-    encoder.fit(hdf5_file["labels"])
-    encoded_data = encoder.transform(hdf5_file["labels"])
+    encoder.fit(y_train)
+    encoded_train = encoder.transform(y_train)
+    encoded_test = encoder.transform(y_test)
 
     # convert integers to dummy variables (i.e. one hot encoded)
-    dummy_y = utils.to_categorical(encoded_data)[:,1:]
-    
+    dummy_train = utils.to_categorical(encoded_train)[:,1:]
+    dummy_test = utils.to_categorical(encoded_test)[:,1:]
 
+
+        
     # Build NN Structure
-    
-    epochs = 20
-    
+    batch_sizes = [8,16,64]
+    filters = [32,16,8]
 
-    p = {
-     #number of neurons for the first layer
-     'first_neuron':[4, 8, 16, 32],
-     'batch_size': [4, 8, 16, 32]
-     }
-    
-    # and run the scan
-    h = ta.Scan(hdf5_file["img"], dummy_y,
-                #calling parameter boundaries
-                params=p,
-                dataset_name='first_test',
-                experiment_no='1',
-                model=CNN_model,
-                #grid_downsample determines what % of permutation will be randomly sampled and scanned
-                #NOTE: repeat - 1% sample, see result, narrow the boundaries in the dictionary - until you get the result you want
-                grid_downsample=0.1,
-                val_split = 0.2)
+    num_classes = 1083
+    epochs = 40
+    input_shape = (120,120, 3)
 
-    # use Scan object as input
-    r = ta.Reporting('first_test_1.csv')
+    results = []
+
+    for batch_size,filter in zip(batch_sizes,filters):
+
+        model = Sequential()
+        model.add(Conv2D(filter, kernel_size=(3, 3),
+                        activation ='relu',
+                        input_shape =input_shape,
+                        padding='same',
+                        dilation_rate=2))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(filter*2, (3, 3), activation='relu', padding='same'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax'))
+        model.compile(loss='categorical_crossentropy',
+                    optimizer=optimizers.Adadelta(),
+                    metrics=['accuracy'])
+        
+        early_stop = EarlyStopping(monitor="val_acc",patience = 5, verbose = 1)
+
+        model.fit(X_train,dummy_train,
+                            validation_data=(X_test,dummy_test),
+                            batch_size = batch_size,
+                            epochs = epochs,
+                            verbose = 2,
+                            shuffle = False,
+                            callbacks=[early_stop]
+                            )
+        result = early_stop.best
+        print("Best result with batch size", batch_size, "filter", filter)
+        print("is",result)
+        results.append(result)
+
+    print("All Record:", results)
+
+
+
+
+   
